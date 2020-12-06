85481446fc00d9b9b4adbdb75a51c8052c2107f7
E019580 <mani.klein@elbitsystems.com>
2020-12-06 16:56:28 +0200

    played a bit with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Kashit1Component } from './components/kashit1/kashit1.component';
+import { Kashit2Component } from './components/kashit2/kashit2.component';
+import { MapComponent } from './components/map/map.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PanoramaComponent } from './components/panorama/panorama.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { SetupComponent } from './layouts/setup/setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {
+    path: 'setup',
+    component: SetupComponent,
+    children: [
+      {
+        path: '',
+        component: MapComponent,
+        outlet: 'left'
+      },
+      {
+        path: 'grids',
+        component: Kashit1Component,
+        outlet: 'top'
+      },
+      {
+        path: 'grids',
+        component: Kashit2Component,
+        outlet: 'bottom'
+      },
+      {
+        path: 'details',
+        component: Kashit2Component,
+        outlet: 'top'
+      },
+      {
+        path: 'details',
+        component: Kashit1Component,
+        outlet: 'bottom'
+      }
+    ]
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanoramaComponent } from './components/panorama/panorama.component';
+import { LayoutsModule } from './layouts/layouts.module';
+import { MapComponent } from './components/map/map.component';
+import { Kashit1Component } from './components/kashit1/kashit1.component';
+import { Kashit2Component } from './components/kashit2/kashit2.component';
+import { MatanComponent } from './components/matan/matan.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanoramaComponent,
+    MapComponent,
+    Kashit1Component,
+    Kashit2Component,
+    MatanComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    LayoutsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
